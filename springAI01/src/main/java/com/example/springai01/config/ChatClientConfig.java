412475feeb73b8a59b9ad2c0fbd8dd604a735281
PankajAjmera1 <coderajmera@gmail.com>
2025-12-28 00:15:07 +0530

    TIll Advisors

---
+++ springAI01/src/main/java/com/example/springai01/config/ChatClientConfig.java
@@ -0,0 +1,29 @@
+package com.example.springai01.config;
+
+import com.example.springai01.advisors.TokenUsageAuditAdvisor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ChatClientConfig {
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder
+//                .defaultAdvisors(new SimpleLoggerAdvisor())
+//                .defaultAdvisors(new TokenUsageAuditAdvisor())
+                .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
+                .defaultSystem("""
+                        You are an internal HR assistant. Your role is to help \s
+                        employees with their HR policies. such as\s
+                        leave policies, work hours, benefits, pay, and code of conduct\s
+
+                        If a user asks for help anything outside of these topics\s
+                        Kindly inform that you can only assist with queries related to HR policies\s
+                        """)
+                .build();
+    }
+}
