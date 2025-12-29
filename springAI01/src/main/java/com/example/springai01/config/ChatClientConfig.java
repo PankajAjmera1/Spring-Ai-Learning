641e74410ba8d73889780acb7e59e3a944f906d2
PankajAjmera1 <coderajmera@gmail.com>
2025-12-29 22:53:32 +0530

    Chat Options | Stream output using flux and stream

--- springAI01/src/main/java/com/example/springai01/config/ChatClientConfig.java
+++ springAI01/src/main/java/com/example/springai01/config/ChatClientConfig.java
@@ -3,6 +3,7 @@ package com.example.springai01.config;
 import com.example.springai01.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,9 +13,12 @@ import java.util.List;
 public class ChatClientConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        ChatOptions chatOptions =  ChatOptions.builder()
+                .temperature(0.8).build();
         return chatClientBuilder
 //                .defaultAdvisors(new SimpleLoggerAdvisor())
 //                .defaultAdvisors(new TokenUsageAuditAdvisor())
+                .defaultOptions(chatOptions)
                 .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
                 .defaultSystem("""
                         You are an internal HR assistant. Your role is to help \s
