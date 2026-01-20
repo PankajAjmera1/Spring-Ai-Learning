36abbb6a3e91a6c1e20500fcd0eeedf8b79bc7a2
PankajAjmera1 <coderajmera@gmail.com>
2026-01-20 23:54:24 +0530

    Added Basic Tools for LLM

---
+++ springAI01/src/main/java/com/example/springai01/tools/TimeTools.java
@@ -0,0 +1,29 @@
+package com.example.springai01.tools;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+@Component
+public class TimeTools {
+
+    private static final Logger logger = LoggerFactory.getLogger(TimeTools.class);
+
+    @Tool(name = "getCurrentLocalTime", description = "Get the current time in the user's timezone")
+    String getCurrentLocalTime() {
+        logger.info("Returning the current time in user's timezone-");
+        return LocalTime.now().toString();
+    }
+
+    @Tool(name = "getCurrentTime", description = "Get the current time in the specified time zone.")
+    public String  getCurrentTime(@ToolParam(description = "Value representing the time zone") String timeZone)
+    {
+        logger.info("Returning the current time in the timezone{}", timeZone);
+        return LocalTime.now(ZoneId.of(timeZone)).toString();
+    }
+}
